c2fea784d48cd25bd714daac16d1946900efa426
cristofer.crego <cregocrisoferlf@gmail.com>
2024-03-13 17:45:45 -0300

    deploy

--- src/db.js
+++ src/db.js
@@ -6,13 +6,10 @@ const ActivityModel = require("./models/Activity");
 const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
-const sequelize = new Sequelize(
-  `${DB_DEPLOY}`,
-  {
-    logging: false,
-    native: false,
-  }
-);
+const sequelize = new Sequelize(DB_DEPLOY, {
+  logging: false,
+  native: false,
+});
 
 const basename = path.basename(__filename);
 
